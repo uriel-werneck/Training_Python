6c26d500cf4bdaf584c2b60ed83ce121a0cc5aa7
Uriel Werneck <131367427+uriel-werneck@users.noreply.github.com>
2023-07-10 20:39:33 -0300

    Update busca_binária_recursiva.py

--- busca_binária_recursiva.py
+++ busca_binária_recursiva.py
@@ -1,21 +1,20 @@
-def busca_binária_recursiva(lista, num):
-
+def busca_binária_recursiva(lista, número):
     if len(lista) == 0: # se a lista estiver vazia
         return False # o número procurado não está na lista
 
     metade = len(lista) // 2 # posição do elemento central da lista
 
-    if num == lista[metade]: # se o número procurado for igual ao número na posição central da lista
+    if número == lista[metade]: # se o número procurado for igual ao número na posição central da lista
         return True # o número procurado está na lista
 
-    elif num > lista[metade]: # se o número procurado é maior que o número na posição central da lista
-        return busca_binária_recursiva(lista[metade + 1:], num) # realiza uma nova busca a partir do segundo elemento da lista
+    elif número > lista[metade]: # se o número procurado é maior que o número na posição central da lista
+        return busca_binária_recursiva(lista[metade + 1:], número) # realiza uma nova busca na metade direita da lista
 
-    elif num < lista[metade]: # se o número procurado é menor que o número na posição central da lista
-        return busca_binária_recursiva(lista[:metade], num) # realiza uma nova busca a partir do primeiro elemento da lista até o penúltimo
+    elif número < lista[metade]: # se o número procurado é menor que o número na posição central da lista
+        return busca_binária_recursiva(lista[:metade], número) # realiza uma nova busca na metade esquerda da lista
 
 
-elementos = [4, 13, 20, 25, 44, 50, 52] # lista que será usada como parâmetro da função
+lista = [4, 13, 20, 25, 44, 50, 52] # lista, em ordem crescente, que será usada como parâmetro da função
 número = int(input('Escolha um número: ')) # número que será buscado na lista
 
-print(busca_binária_recursiva(elementos, número)) # apresentação do resultado da busca binária recursiva
+print(busca_binária_recursiva(lista, número)) # apresenta o resultado da busca binária recursiva
