4fc062c72786b964a22ed9c3716d6265aaf9e70c
Uriel Werneck de Alcântara <131367427+uriel-werneck@users.noreply.github.com>
2023-07-21 02:14:43 -0300

    atualização de comentários

--- busca_em_largura.py
+++ busca_em_largura.py
@@ -1,4 +1,4 @@
-# Dicionário contendo os vértices e suas ligações
+# dicionário contendo os vértices e suas ligações
 vértices = {
     'A': ['B', 'C', 'D'],
     'B': [],
@@ -9,8 +9,8 @@ vértices = {
     'G': []
 }
 
-# Dicionário contendo o STATUS de cada vértice
-STATUS = {
+# dicionário contendo o status de cada vértice
+status = {
     'A': 1,
     'B': 1,
     'C': 1,
@@ -20,32 +20,32 @@ STATUS = {
     'G': 1
 }
 
-# Listas que receberão os vértices
-FILA = []
+# listas que receberão os vértices
+fila = []
 processados = []
 
-# Cria a função que realiza a Busca em Largura
-def busca_em_largura(vértices, STATUS, nó):
+# cria a função que realiza a Busca em Largura
+def busca_em_largura(vértices, status, nó):
 
-    FILA.append(nó)  # Coloca o vértice inicial na FILA
-    STATUS[nó] = 2  # muda o STATUS do vértice inicial para 2
+    fila.append(nó)  # coloca o vértice inicial na fila
+    status[nó] = 2  # muda o status do vértice inicial para 2
 
-    while FILA:  # Até a FILA estar vazia
-        primeiro = FILA.pop(0)  # Remove o primeiro elemento da FILA
-        processados.append(primeiro)  # Adiciona o primeiro elemento na lista de processados
-        STATUS[primeiro] = 3  # muda o STATUS do primeiro elemento para 3
+    while fila:  # enquanto houver elementos na fila
+        primeiro = fila.pop(0)  # remove o primeiro elemento da fila
+        processados.append(primeiro)  # adiciona o primeiro elemento na lista de processados
+        status[primeiro] = 3  # muda o status do primeiro elemento para 3
 
-        for vizinho in vértices[primeiro]:  # Para cada vizinho do primeiro elemento da FILA
-            if STATUS[vizinho] == 1:  # Se o STATUS do vizinho for igual a 1
-                FILA.append(vizinho)  # Adicione o vizinho ao final da FILA
-                STATUS[vizinho] = 2  # Mude o STATUS do vizinho para 2
+        for vizinho in vértices[primeiro]:  # para cada vizinho do primeiro elemento da fila
+            if status[vizinho] == 1:  # se o status do vizinho for igual a 1
+                fila.append(vizinho)  # adicione o vizinho ao final da fila
+                status[vizinho] = 2  # mude o status do vizinho para 2
 
-            elif STATUS[vizinho] == 2 or STATUS[vizinho] == 3:  # Se o STATUS do vizinho for igual a 2 ou 3
-                continue  # Ignore esse vizinho
+            elif status[vizinho] == 2 or status[vizinho] == 3:  # se o status do vizinho for igual a 2 ou 3
+                continue  # ignore esse vizinho
 
-# Usa a função que realiza a Busca em Largura
-busca_em_largura(vértices, STATUS, 'A') # O vértice "A" é definido como nó inicial
+# chama a função que realiza a Busca em Largura
+busca_em_largura(vértices, status, 'A') # o vértice "A" é definido como nó inicial
 
-# Apresenta o resultado da Busca em Largura
+# apresenta o resultado da Busca em Largura
 print('Resultado da Busca em Largura:')
 print(processados)
