e9bcf6ae120040f1290aab4c3ebf7a87a80233fd
Uriel Werneck de Alcântara <131367427+uriel-werneck@users.noreply.github.com>
2023-08-02 15:39:25 -0300

    Updating comments

--- bubble_sort.py
+++ bubble_sort.py
@@ -5,10 +5,10 @@ print('Unsorted list:', my_list)
 flag = 1  # flag to start the while loop
 
 while flag != 0:  # while flag is different from 0
-    flag = 0  # if no values are swapped, the loop will be terminated
+    flag = 0  # if no values are swapped, the loop will end
     for i in range(1, len(my_list)):  # check each element in the list starting from the second one
         if my_list[i] < my_list[i - 1]:  # if the value ahead is smaller than the value behing
             my_list[i], my_list[i - 1] = my_list[i - 1], my_list[i]  # swap the values
-            flag = 1  # if any value is swapped, the loops restarts
+            flag = 1  # if any value is swapped, the loop restarts
 
 print('Sorted list:', my_list)
